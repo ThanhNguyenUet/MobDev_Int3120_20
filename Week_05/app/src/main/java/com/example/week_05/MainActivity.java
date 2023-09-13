594b3223c7173aa6cefde6fbf6bf743615486bc2
congthanh6g <65008703+congthanh6g@users.noreply.github.com>
2023-09-14 02:06:50 +0700

    week05

---
+++ Week_05/app/src/main/java/com/example/week_05/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.week_05;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends Activity {
+
+    DateFormat fmtDateAndTime = DateFormat.getDateTimeInstance();
+    TextView lblDateAndTime;
+    Calendar myCalendar = Calendar.getInstance();
+
+    DatePickerDialog.OnDateSetListener d = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+            // i = year , i1 = month, i2 = day
+            myCalendar.set(Calendar.YEAR, i);
+            myCalendar.set(Calendar.MONTH, i1);
+            myCalendar.set(Calendar.DAY_OF_MONTH, i2);
+        }
+    };
+
+    TimePickerDialog.OnTimeSetListener t = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker timePicker, int i, int i1) {
+            // timepicker = view, i = hour of day, i1 = minute
+            myCalendar.set(Calendar.HOUR_OF_DAY, i);
+            myCalendar.set(Calendar.MINUTE, i1);
+            //updateLabel();
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnDate = findViewById(R.id.btnDate);
+        Button btnTime = findViewById(R.id.btnTime);
+
+        btnDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(MainActivity.this, d,
+                        myCalendar.get(Calendar.YEAR),
+                        myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+
+    }
+}
